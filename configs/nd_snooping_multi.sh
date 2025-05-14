c25ebdf9091bf69d9f578245053d3a409382fc48
Ernesto Sánchez <esanchez@ucasal.edu.ar>
2025-05-14 00:15:52 -0300

    Update nd_snooping_multi.sh

--- configs/nd_snooping_multi.sh
+++ configs/nd_snooping_multi.sh
@@ -12,13 +12,12 @@ echo "[*] Capturando mensajes ND (NS/NA) durante ${CAPTURE_DURATION} segundos...
 PIDS=()
 for IFACE in "${INTERFACES[@]}"; do
     FILE="$TMP_DIR/$IFACE.pcap"
-    # Captura con timeout por interfaz
     timeout "$CAPTURE_DURATION" tcpdump -i "$IFACE" -w "$FILE" \
-        'icmp6 and (ip6[40] == 135 or ip6[40] == 136)' &
+        'icmp6 and (ip6[40] == 135 or ip6[40] == 136)' &> /dev/null &
     PIDS+=($!)
 done
 
-# Espera que todos terminen
+# Espera que todas las capturas terminen
 for PID in "${PIDS[@]}"; do
     wait "$PID"
 done
@@ -28,7 +27,7 @@ declare -a ALL_BINDINGS=()
 
 for IFACE in "${INTERFACES[@]}"; do
     FILE="$TMP_DIR/$IFACE.pcap"
-    
+
     # Validar que haya al menos 1 paquete en el archivo
     PACKET_COUNT=$(tcpdump -nn -r "$FILE" 2>/dev/null | wc -l)
     if [[ "$PACKET_COUNT" -eq 0 ]]; then
@@ -36,17 +35,16 @@ for IFACE in "${INTERFACES[@]}"; do
         continue
     fi
 
-    # Procesar solo si hay tráfico ND real
     while read -r line; do
         SRC_MAC=""
         IPV6=""
 
-        # Extraer MAC (la primera encontrada)
+        # Extraer MAC
         if [[ "$line" =~ ([0-9a-fA-F]{2}(:[0-9a-fA-F]{2}){5}) ]]; then
             SRC_MAC="${BASH_REMATCH[1],,}"
         fi
 
-        # Extraer IPv6 (NS: who has / NA: tgt is)
+        # Extraer IPv6
         if [[ "$line" =~ who\ has\ ([0-9a-fA-F:]+) ]]; then
             IPV6="${BASH_REMATCH[1],,}"
         elif [[ "$line" =~ tgt\ is\ ([0-9a-fA-F:]+) ]]; then
@@ -64,13 +62,21 @@ for IFACE in "${INTERFACES[@]}"; do
                 '{mac: $mac, ipv6: $ip, interface: $intf, timestamp: $ts}')
             ALL_BINDINGS+=("$BINDING_JSON")
         fi
-
     done < <(tcpdump -nn -r "$FILE" -e 'icmp6 and (ip6[40] == 135 or ip6[40] == 136)' 2>/dev/null)
 done
 
-# Consolidar y eliminar duplicados por IP + interfaz
+# Consolidar y eliminar duplicados por combinación IPv6 + MAC
 if [ ${#ALL_BINDINGS[@]} -gt 0 ]; then
-    printf '%s\n' "${ALL_BINDINGS[@]}" | jq -s '{"bindings": (unique_by(.ipv6, .interface))}' > "$BINDING_FILE"
+    printf '%s\n' "${ALL_BINDINGS[@]}" | jq -s '
+      {
+        bindings: (
+          [ .[] | {key: (.ipv6 + "_" + .mac), value: .} ]
+          | from_entries
+          | to_entries
+          | map(.value)
+        )
+      }
+    ' > "$BINDING_FILE"
 else
     echo '{"bindings": []}' > "$BINDING_FILE"
 fi
